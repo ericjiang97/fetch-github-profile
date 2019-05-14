8e53227a23e7192c019ec71fb272d875bd9e930b
Eric <eric.jiang@monash.edu>
2019-05-15 09:46:56 +1000

    updating files

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # fetch-git
 Fetching Git Profile into Your Terminal
 
-# Installation
+# Package Usage 
+
+## Installation
 To install this run
 ```bash
 npm install fetch-github-profile -g
@@ -9,8 +11,11 @@ npm install fetch-github-profile -g
 yarn global add fetch-github-profile
 ```
 
-# Running
+## Running
 To run simply type `fetch-github-profile --user="<github-user-name-here>"
+
+## Developing
+Simply clone this repository and use `yarn develop` or `npm run develop` to test it out. You may need to pass arguments in.
 
 # LICENSE
 This is licensed under `MIT`
--- index.js
+++ index.js
@@ -2,44 +2,51 @@
 
 "use strict";
 
-const axios = require('axios')
-const commandLineArgs = require('command-line-args')
+const axios = require("axios");
+const commandLineArgs = require("command-line-args");
 
-const optionDefinitions = [
-    { name: 'user', alias: 'u', type: String },
-  ]
+const optionDefinitions = [{ name: "user", alias: "u", type: String }];
 
-const MAX_MIDDLE = 80
+const MAX_MIDDLE = 80;
 
 const LABELS = {
-    name: ' Name:',
-    github_profile: ' GitHub Profile:',
-    company: ' Company:'
-}
-const options = commandLineArgs(optionDefinitions)
+  name: " Name:",
+  github_profile: " GitHub Profile:",
+  company: " Company:"
+};
+const options = commandLineArgs(optionDefinitions);
 
-if(options.user === null || options.user === undefined){
-    console.error('An occured has occured, please pass in a user under --user="username"')
+if (options.user === null || options.user === undefined) {
+  console.error(
+    'An occured has occured, please pass in a user under --user="username"'
+  );
+  process.exit(1);
+}
+
+axios
+  .get(`https://api.github.com/users/${options.user}`)
+  .then(resp => resp.data)
+  .then(data => {
+    const { name, html_url, company } = data;
+
+    const MAX_WHITESPACE = MAX_MIDDLE - 2;
+
+    const nameWithLabel = `${LABELS.name}${" ".repeat(
+      MAX_WHITESPACE - LABELS.name.length - name.length
+    )}${name}  `;
+    const githubProfile = `${LABELS.github_profile}${" ".repeat(
+      MAX_WHITESPACE - LABELS.github_profile.length - html_url.length
+    )}${html_url}  `;
+    const companyProfile = `${LABELS.company}${" ".repeat(
+      MAX_WHITESPACE - LABELS.company.length - (company ? company.length : 0)
+    )}${company || ""}  `;
+    console.log(" ".repeat(5) + "-".repeat(MAX_MIDDLE));
+    name && console.log(" ".repeat(4) + "|" + nameWithLabel + "|");
+    githubProfile && console.log(" ".repeat(4) + "|" + githubProfile + "|");
+    companyProfile && console.log(" ".repeat(4) + "|" + companyProfile + "|");
+    console.log(" ".repeat(5) + "-".repeat(MAX_MIDDLE));
+  })
+  .catch(error => {
+    console.error(`${error.response.status}: ${error.response.statusText}`);
     process.exit(1);
-}
-
-axios.get(`https://api.github.com/users/${options.user}`)
-    .then(resp => resp.data)
-    .then(data => {
-        const { name, html_url, company } = data
-
-        const MAX_WHITESPACE = MAX_MIDDLE - 2
-        
-        const nameWithLabel = `${LABELS.name}${' '.repeat(MAX_WHITESPACE-LABELS.name.length-name.length)}${name}  `
-        const githubProfile = `${LABELS.github_profile}${' '.repeat(MAX_WHITESPACE-LABELS.github_profile.length-html_url.length)}${html_url}  `
-        const companyProfile = `${LABELS.company}${' '.repeat(MAX_WHITESPACE-LABELS.company.length-(company ? company.length : 0))}${company || ''}  `
-        console.log(' '.repeat(5) + '-'.repeat(MAX_MIDDLE))
-        name && console.log(' '.repeat(4) + '|' + nameWithLabel + '|')
-        githubProfile && console.log(' '.repeat(4) + '|' + githubProfile + '|')
-        companyProfile && console.log(' '.repeat(4) + '|' + companyProfile + '|')
-        console.log(' '.repeat(5) + '-'.repeat(MAX_MIDDLE))
-
-    }).catch(error => {
-        console.error(`${error.response.status}: ${error.response.statusText}`);
-        process.exit(1);
-    })
+  });
