469ee9421860b6eb8390b4d9f2387869918ec719
Eric <eric.jiang@monash.edu>
2019-05-14 15:49:46 +1000

    more work done :)

--- src/index.js
+++ src/index.js
@@ -3,27 +3,35 @@
 "use strict";
 
 const axios = require('axios')
+const commandLineArgs = require('command-line-args')
+
+const optionDefinitions = [
+    { name: 'user', alias: 'u', type: String },
+  ]
 
 const MAX_MIDDLE = 80
 
 const LABELS = {
     name: ' Name:',
-    github_profile: ' GitHub Profile:'
+    github_profile: ' GitHub Profile:',
+    company: ' Company:'
 }
-console.log(process.argv)
+const options = commandLineArgs(optionDefinitions)
 
-axios.get('https://api.github.com/users/lorderikir')
+axios.get(`https://api.github.com/users/${options.user}`)
     .then(resp => resp.data)
     .then(data => {
-        const { name, html_url } = data
+        const { name, html_url, company } = data
 
         const MAX_WHITESPACE = MAX_MIDDLE - 2
         
         const nameWithLabel = `${LABELS.name}${' '.repeat(MAX_WHITESPACE-LABELS.name.length-name.length)}${name}  `
         const githubProfile = `${LABELS.github_profile}${' '.repeat(MAX_WHITESPACE-LABELS.github_profile.length-html_url.length)}${html_url}  `
+        const companyProfile = `${LABELS.company}${' '.repeat(MAX_WHITESPACE-LABELS.company.length-(company ? company.length : 0))}${company}  `
         console.log(' '.repeat(5) + '-'.repeat(MAX_MIDDLE))
         name && console.log(' '.repeat(4) + '|' + nameWithLabel + '|')
         githubProfile && console.log(' '.repeat(4) + '|' + githubProfile + '|')
+        companyProfile && console.log(' '.repeat(4) + '|' + companyProfile + '|')
         console.log(' '.repeat(5) + '-'.repeat(MAX_MIDDLE))
 
     })
