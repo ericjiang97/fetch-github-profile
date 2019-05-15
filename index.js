d77ad4a459fdba73d4fb2b6f95a8c8fef1276f5e
Eric <eric.jiang@monash.edu>
2019-05-15 10:16:31 +1000

    v0.0.5 updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 const axios = require("axios");
 const commandLineArgs = require("command-line-args");
+const inquirer = require("inquirer");
 
 const optionDefinitions = [{ name: "user", alias: "u", type: String }];
 
@@ -16,37 +17,59 @@ const LABELS = {
 };
 const options = commandLineArgs(optionDefinitions);
 
-if (options.user === null || options.user === undefined) {
-  console.error(
-    'An occured has occured, please pass in a user under --user="username"'
-  );
-  process.exit(1);
+function promptAndGetUserName() {
+  if (options.user === null || options.user === undefined) {
+    return inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "username",
+          message: "enter a username"
+        }
+      ])
+      .then(resp => {
+        return resp.username;
+      });
+  } else {
+    return new Promise((resolve, reject) => resolve(options.user));
+  }
 }
+promptAndGetUserName().then(username => {
+  axios
+    .get(`https://api.github.com/users/${username}`)
+    .then(resp => resp.data)
+    .then(data => {
+      const { name, html_url, company, login } = data;
 
-axios
-  .get(`https://api.github.com/users/${options.user}`)
-  .then(resp => resp.data)
-  .then(data => {
-    const { name, html_url, company } = data;
+      const MAX_WHITESPACE = MAX_MIDDLE - 2;
 
-    const MAX_WHITESPACE = MAX_MIDDLE - 2;
+      let gitHubName = name || login;
 
-    const nameWithLabel = `${LABELS.name}${" ".repeat(
-      MAX_WHITESPACE - LABELS.name.length - name.length
-    )}${name}  `;
-    const githubProfile = `${LABELS.github_profile}${" ".repeat(
-      MAX_WHITESPACE - LABELS.github_profile.length - html_url.length
-    )}${html_url}  `;
-    const companyProfile = `${LABELS.company}${" ".repeat(
-      MAX_WHITESPACE - LABELS.company.length - (company ? company.length : 0)
-    )}${company || ""}  `;
-    console.log(" ".repeat(5) + "-".repeat(MAX_MIDDLE));
-    name && console.log(" ".repeat(4) + "|" + nameWithLabel + "|");
-    githubProfile && console.log(" ".repeat(4) + "|" + githubProfile + "|");
-    companyProfile && console.log(" ".repeat(4) + "|" + companyProfile + "|");
-    console.log(" ".repeat(5) + "-".repeat(MAX_MIDDLE));
-  })
-  .catch(error => {
-    console.error(`${error.response.status}: ${error.response.statusText}`);
-    process.exit(1);
-  });
+      const nameWithLabel = `${LABELS.name}${" ".repeat(
+        MAX_WHITESPACE -
+          LABELS.name.length -
+          (gitHubName ? gitHubName.length : 0)
+      )}${gitHubName}  `;
+
+      const githubProfile = `${LABELS.github_profile}${" ".repeat(
+        MAX_WHITESPACE -
+          LABELS.github_profile.length -
+          (html_url ? html_url.length : 0)
+      )}${html_url}  `;
+
+      const companyProfile = `${LABELS.company}${" ".repeat(
+        MAX_WHITESPACE - LABELS.company.length - (company ? company.length : 0)
+      )}${company || ""}  `;
+
+      // Print Card
+      console.log(" ".repeat(5) + "-".repeat(MAX_MIDDLE));
+      gitHubName && console.log(" ".repeat(4) + "|" + nameWithLabel + "|");
+      githubProfile && console.log(" ".repeat(4) + "|" + githubProfile + "|");
+      companyProfile && console.log(" ".repeat(4) + "|" + companyProfile + "|");
+      console.log(" ".repeat(5) + "-".repeat(MAX_MIDDLE));
+    })
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+});
